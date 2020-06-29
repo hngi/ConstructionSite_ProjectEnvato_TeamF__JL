e03411d8e254adbfe8c181199acaddb9dbe4f3b3
Taiwo Oyewole <oyewoletaiwo94@gmail.com>
2020-06-29 13:42:01 +0100

    build construction page

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  let x = document.getElementById('mytopnav');
+
+  if (x.className === 'topnav') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'topnav';
+  }
+}
