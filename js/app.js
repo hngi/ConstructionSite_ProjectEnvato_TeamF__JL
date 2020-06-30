f8839cc9acfb39b05eb8304964a96e06481997d9
Taiwo Oyewole <oyewoletaiwo94@gmail.com>
2020-06-30 17:37:06 +0100

    update changes

--- js/app.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-  let x = document.getElementById('mytopnav');
-
-  if (x.className === 'topnav') {
-    x.className += ' responsive';
-  } else {
-    x.className = 'topnav';
-  }
-}
