fb912003f20cb7ed418d151f6b37d1cb3fa948e2
Jeth0214 <rolandjethrosuyom@yahoo.com>
2020-07-01 18:21:39 +0300

    links bug fix

--- js/app.js
+++ js/app.js
@@ -44,6 +44,21 @@
 })(jQuery); // End of use strict
 
 
+// function to call whyen id bookmark doesn't work properly
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
  
 
 $(window).load(function () {
